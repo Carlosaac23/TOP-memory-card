a4afe227d2803e5c046ad8922bc161e898a1821f
Carlos <carlosaac232001@gmail.com>
2025-12-25 16:57:06 -0500

    chore: import new components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import Cards from './components/Cards.jsx';
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <main className='flex min-h-screen flex-col'>
+      <Header />
+      <Cards />
+      <Footer />
+    </main>
   </StrictMode>,
 );
