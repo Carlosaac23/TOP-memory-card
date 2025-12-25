9992f8e37e5e62c6301513b896dd3feca2377754
Carlos <carlosaac232001@gmail.com>
2025-12-25 16:58:24 -0500

    chore: change Cards.jsx name to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import Card from './Card';
 
-export default function Cards() {
+export default function Main() {
   return (
     <section>
       <div className='flex gap-2 p-4'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Cards from './components/Cards.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <main className='flex min-h-screen flex-col'>
       <Header />
-      <Cards />
+      <Main />
       <Footer />
     </main>
   </StrictMode>,
