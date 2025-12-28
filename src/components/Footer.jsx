16e2827ffbbece1c76b294388bd7339e413b69ac
Carlos <carlosaac232001@gmail.com>
2025-12-27 20:15:44 -0500

    feat: create Footer component with important and basic information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
-    <footer className='mt-auto bg-red-300'>
-      <p>Soy el footer</p>
+    <footer className='font-jersey mt-auto bg-neutral-700 p-4 text-neutral-50'>
+      <p className='text-center text-lg font-light tracking-wide'>
+        Rights Reserved &copy;{' '}
+        <a
+          className='hover:underline hover:underline-offset-2'
+          href='https://portfolio-steel-kappa-78.vercel.app/'
+          target='_blank'
+        >
+          Carlos Acosta
+        </a>{' '}
+        {new Date().getFullYear()}{' '}
+      </p>
     </footer>
   );
 }
