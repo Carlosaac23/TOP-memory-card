371a46acc70b85870b55fbc88c0797fdd26309cd
Carlos <carlosaac232001@gmail.com>
2025-12-27 21:39:55 -0500

    feat: - delete console.log statements - destructure id from each pokemon and add it to each object - use each object id to Card component key property

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,21 +5,21 @@ import Card from './Card';
 
 export default function Main() {
   const [objects, setObjects] = useState([]);
-  console.log(objects);
 
   useEffect(() => {
     async function fetchPokemon() {
       for (let i = 0; i < 10; i++) {
         const randomID = getRandomID();
         const randomPokemon = await pokemons(randomID);
-        console.log(randomPokemon);
         const {
           sprites: { front_default },
           name,
+          id,
         } = randomPokemon;
+
         setObjects((prevURLs) => [
           ...prevURLs,
-          { pokemonName: name, imageURL: front_default },
+          { id, pokemonName: name, imageURL: front_default },
         ]);
       }
     }
@@ -30,8 +30,10 @@ export default function Main() {
   return (
     <section>
       <div className='flex flex-wrap gap-2 p-4'>
-        {objects.map(({ pokemonName, imageURL }) => {
-          return <Card imageURL={imageURL} pokemonName={pokemonName} />;
+        {objects.map(({ id, pokemonName, imageURL }) => {
+          return (
+            <Card key={id} imageURL={imageURL} pokemonName={pokemonName} />
+          );
         })}
       </div>
     </section>
