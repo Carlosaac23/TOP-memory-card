3360ef3942e19a59e48ceb8c13cd067d5c246142
Carlos <carlosaac232001@gmail.com>
2025-12-27 21:39:46 -0500

    chore: use capitalize function to render capitalize pokemons names

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,10 @@
+import { capitalize } from '../utils';
+
 export default function Card({ imageURL, pokemonName }) {
   return (
     <div className='rounded-md border border-neutral-700 p-2'>
       <img src={imageURL} alt='' />
-      <h2>{pokemonName}</h2>
+      <h2>{capitalize(pokemonName)}</h2>
     </div>
   );
 }
