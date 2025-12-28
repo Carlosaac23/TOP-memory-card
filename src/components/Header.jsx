1b3514173e4c6f9e55acee09f5364b5101dfeff1
Carlos <carlosaac232001@gmail.com>
2025-12-27 20:16:25 -0500

    feat: create Header component with game name and profile pic of my github account

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 export default function Header() {
   return (
-    <header className='flex justify-between bg-sky-300'>
-      <p>Logo</p>
-      <h1>Hola</h1>
+    <header className='flex items-center justify-between bg-neutral-700 p-4'>
+      <h1 className='font-jersey text-5xl tracking-wide text-neutral-50'>
+        Memory Card Game
+      </h1>
+      <a href='https://portfolio-steel-kappa-78.vercel.app/' target='_blank'>
+        <img
+          className='w-10 rounded-full'
+          src='https://avatars.githubusercontent.com/u/127145057?v=4'
+          alt='Profile pic of my personal GitHub account'
+        />
+      </a>
     </header>
   );
 }
