63cf1451bcb4f49e006e9fee579a832fee0436af
Carlos <carlosaac232001@gmail.com>
2026-01-16 12:13:07 -0500

    chore: format code with oxfmt

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
+import './index.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +14,5 @@ createRoot(document.getElementById('root')).render(
       <Main />
       <Footer />
     </main>
-  </StrictMode>,
+  </StrictMode>
 );
