068c5fa16efcbf36bf58f4fa6918d7f414a97f48
Carlos <carlosaac232001@gmail.com>
2026-01-16 12:13:21 -0500

    refactor: - delete all react hooks function so custom hook manage them - destructure variables and function from usePokemons custom hook - add a spinner when fetching pokemons - change destructure variables

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,55 +1,12 @@
-import { shuffle } from 'lodash';
-import { useEffect, useState } from 'react';
-import fetchPokemons from '../lib/fetch-pokemons';
-import { getRandomID } from '../utils';
+import { BeatLoader } from 'react-spinners';
+
+import { usePokemons } from '../hooks/usePokemons';
+
 import Card from './Card';
 
 export default function Main() {
-  const [objects, setObjects] = useState([]);
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [lastCard, setLastCard] = useState(null);
-
-  useEffect(() => {
-    async function fetchPokemon() {
-      for (let i = 0; i < 12; i++) {
-        const randomID = getRandomID();
-        const randomPokemon = await fetchPokemons(randomID);
-        const {
-          sprites: { front_default },
-          name,
-          id,
-        } = randomPokemon;
-
-        setObjects((prevURLs) => [
-          ...prevURLs,
-          { id, pokemonName: name, imageURL: front_default },
-        ]);
-      }
-    }
-
-    fetchPokemon();
-  }, []);
-
-  function handleCardClick(pokemonId) {
-    const newObjects = shuffle([...objects]);
-
-    if (pokemonId === lastCard) {
-      setScore(0);
-      setLastCard(null);
-      setObjects(newObjects);
-
-      if (score > bestScore) {
-        setBestScore(score);
-      }
-
-      return;
-    }
-
-    setScore((prevScore) => prevScore + 1);
-    setLastCard(pokemonId);
-    setObjects(newObjects);
-  }
+  const { pokemons, score, bestScore, handleCardClick, isLoading } =
+    usePokemons();
 
   return (
     <section>
@@ -58,17 +15,19 @@ export default function Main() {
         <p>Best Score: {bestScore}</p>
       </div>
       <div className='flex flex-wrap justify-center gap-6 p-4'>
-        {objects.map(({ id, pokemonName, imageURL }) => {
-          return (
-            <Card
-              id={id}
-              handleClick={handleCardClick}
-              key={id}
-              imageURL={imageURL}
-              pokemonName={pokemonName}
-            />
-          );
-        })}
+        {isLoading && <BeatLoader color='#404040' />}
+        {pokemons &&
+          pokemons.map(({ id, name, sprites: { front_default } }) => {
+            return (
+              <Card
+                id={id}
+                handleClick={handleCardClick}
+                key={id}
+                imageURL={front_default}
+                pokemonName={name}
+              />
+            );
+          })}
       </div>
     </section>
   );
